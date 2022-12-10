7b2f1ac372be0ac7b16f118ab7e6cb8243086342
srinivasab666 <srinivasab@yahoo.com>
2022-12-10 22:21:42 +0530

    adding logger

--- src/main/java/com/ecoat/management/ECOATSpringApplication/controller/LoginController.java
+++ src/main/java/com/ecoat/management/ECOATSpringApplication/controller/LoginController.java
@@ -4,18 +4,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.ecoat.management.ECOATSpringApplication.dto.LoginRequest;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/api/auth")
 @AllArgsConstructor
+@Slf4j
 public class LoginController {
 
 	@PostMapping("/login")
 	public void login(@RequestBody LoginRequest loginRequest) {
-
+		System.out.println("test =========");
+		log.info("Enter");
 	}
-	
+
 }
